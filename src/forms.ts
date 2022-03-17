cac67799c9c40fee9d4b0e7d2026081d28d6062d
Cole <cole@colevscode.io>
2022-03-17 15:58:14 -0700

    adding FormError and type narrowing functions

--- src/forms.ts
+++ src/forms.ts
@@ -6,20 +6,34 @@ export interface SubmissionOptions {
   fetchImpl?: typeof fetch;
 }
 
+export interface FormError {
+  field?: string;
+  code: string | null;
+  message: string;
+}
+
+export interface FieldError extends FormError {
+  field: string;
+}
+
+export function isFieldError(error: FormError): error is FieldError {
+  return (error as FieldError).field !== undefined;
+}
+
 export interface SuccessBody {
   id: string;
   data: object;
 }
 
 export interface ErrorBody {
-  errors: Array<{
-    field?: string;
-    code: string | null;
-    message: string;
-  }>;
+  errors: FormError[];
 }
 
 export type SubmissionBody = SuccessBody | ErrorBody;
+
+export function hasErrors(body: SubmissionBody): body is ErrorBody {
+  return (body as ErrorBody).errors !== undefined;
+}
 
 export interface SubmissionResponse {
   body: SubmissionBody;
